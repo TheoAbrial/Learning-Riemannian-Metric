03b33453de4aaeef7d38995170b0b72e4b7b4fa4
Litotel <95126864+Litotel@users.noreply.github.com>
2024-06-28 20:10:38 +0200

    Update geo.py

--- geo.py
+++ geo.py
@@ -1,27 +1,34 @@
 import torch
 import torchode as to
 import matplotlib.pyplot as plt
-
+from torchdiffeq import odeint
+from torch import nn
 
 #g(x) = [g_11(x) , g_22(x),... , g_nn(x)]
-def exp_para_lin(param_metric,p0,v0,w_i,t_i):
+def exp_para_lin(param_metric,t0,p0,v0,w_i,t_i):
     #print("inside exp_para")
     #print("t_i",t_i)
     #print("p0",p0)
     #print("v0",v0)
     #print("w_i",w_i)
-    exp_para = torch.vmap(lambda t:p0+w_i+v0*t)(t_i)
+    def lin(t):
+        #print(t)
+        return p0+w_i+v0*(t-t0)
+    exp_para = torch.vmap(lin)(t_i)
     return exp_para
 
-def exp_para_logistic(param_metric,p0,v0,w_i,t_i):
-    exp_para = torch.vmap(lambda t:1/(1 + (1/p0 - 1)*torch.exp(-v0*t/(p0*(1-p0)))))(t_i)
+
+def exp_para_logistic(param_metric,t0,p0,v0,w_i,t_i):
+    exp_para = torch.vmap(lambda t:1/(1 + (1/p0 - 1)*torch.exp(-v0*(t-t0)/(p0*(1-p0)))))(t_i)
     return exp_para
 
-def exp_para_poly1D(param_metric,p0,v0,w_i,t_i):
+'''
+ESSAIS AVEC LES SOLVEURS D'ODE
+
+def exp_para_poly1D(param_metric,t0,p0,v0,w_i,t_i):
     g = poly_metric(param_metric)
-    geo = geodesic_1D(p0[0],v0[0],g,t_i)
+    geo = geodesic_1D_pointwise(t0,p0,v0,g,t_i)
     return geo
-
 
 def compute_poly(coeffs,deg,x):
      x_pow = torch.exp(torch.arange(0,deg+1,1) * torch.log(x))
@@ -33,9 +40,9 @@ def poly_metric(param_metric):
         return 1/compute_poly(param_metric,deg,x).reshape((1,1))
     return g
 
-def geodesic_1D(p0,v0,g,t):
+def geodesic_1D(t0,p0,v0,g,t):
     F = lambda t,y:v0 * g(y)/g(p0)
-    
+
     #le solver prend un tableau trié sans doublons
     #on trie
     t=torch.cat((t,torch.zeros(1)))
@@ -58,14 +65,41 @@ def geodesic_1D(p0,v0,g,t):
 
     return gamma
 
+def geodesic_1D_point(t0,y0,v0,g):
+    F = lambda t,y:v0 * g(y)/g(p0)
+    def f(t_eval):
+       term = to.ODETerm(F)
+       step_method = to.Dopri5(term=term)
+       step_size_controller = to.IntegralController(atol=1e-6, rtol=1e-3, term=term)
+       solver = to.AutoDiffAdjoint(step_method, step_size_controller)
+       jit_solver = torch.compile(solver)
+       sol = jit_solver.solve(to.InitialValueProblem(y0=y0, t_eval=t_eval))
+       return sol.ys[0][1]
+    return f
 
-p0 = torch.tensor([0.8])
-v0 = torch.tensor([2.])
-y0 = p0.reshape((1,1))
-param_metric = torch.tensor([1.,0.,0.])
-exp_para_poly1D(param_metric,p0,v0,0,torch.tensor([.1,.2,.3,.4,1.5]))
 
+def eval_geodesic_1D(t0,y0,v0,g):
+    F = lambda t,y:v0 * g(y)/g(p0)
+    def f(t_eval):
 
+       result = odeint(F,y0,t_eval)
+       return result[1]
+    return f
+
+def  geodesic_1D_pointwise(t0,p0,v0,g,t_i):
+    y0 = p0.reshape((1,1))
+    f = geodesic_1D_point(t0,y0,v0,g)
+    print("t_i",t_i)
+    print("t0",t0)
+    n_i = len(t_i)
+    t_eval = torch.vmap(lambda t:torch.linspace(t0,t,2))(t_i)
+    print("t_eval",t_eval)
+
+    geo = torch.zeros((n_i,1))
+    for i in range(n_i):
+        t_eval = torch.tensor([[t0,t_i[i]]])
+        geo[i] = f(t_eval)
+    return geo
 
 def compute_F(param_metric,dim_geo):
     g = poly_metric(param_metric)
@@ -87,6 +121,7 @@ def compute_F(param_metric,dim_geo):
         return torch.cat((F_x,F_alpha))
 
     return F
+
 
 def geo_hamilton(param_metric, t, p0, v0):
     #Résoud les équations de Hamilton.
@@ -120,3 +155,84 @@ def geo_hamilton(param_metric, t, p0, v0):
 
     return gamma
 
+'''
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self,dim_geo,nb_neurones):
+
+        self.nb_neurones = nb_neurones
+        self.dim_geo = dim_geo
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(dim_geo, nb_neurones),
+            nn.Sigmoid(),
+            nn.Linear(nb_neurones, nb_neurones),
+            nn.Sigmoid(),
+            nn.Linear(nb_neurones, nb_neurones),
+            nn.Sigmoid(),
+            nn.Linear(nb_neurones, dim_geo),
+        )
+
+    def init_id(self):
+        #couche 1
+        nn.init.zeros_(self.linear_relu_stack[0].weight)
+        with torch.no_grad():
+            for i in range(self.dim_geo):
+                self.linear_relu_stack[0].weight[i, i] = 1
+        nn.init.zeros_(self.linear_relu_stack[0].bias)
+        
+        #couche 2
+        nn.init.eye_(self.linear_relu_stack[2].weight)
+        nn.init.zeros_(self.linear_relu_stack[2].bias)
+
+        nn.init.eye_(self.linear_relu_stack[4].weight)
+        nn.init.zeros_(self.linear_relu_stack[4].bias)
+
+        #couche 3
+        nn.init.zeros_(self.linear_relu_stack[6].weight)
+        with torch.no_grad():
+            for i in range(self.dim_geo):
+                self.linear_relu_stack[6].weight[i, i] = 1
+        nn.init.zeros_(self.linear_relu_stack[6].bias)    
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+    def extrait_param(self):
+        parameters_tensor = torch.cat([param.view(-1) for param in self.parameters()])
+        return parameters_tensor
+    
+    def import_param(self,new_param):
+        start = 0
+        new_parameters_list = []
+        for param in self.parameters():
+            end = start + param.numel()  # Nombre total d'éléments dans ce paramètre
+            new_parameters_list.append(new_param[start:end].view(param.size()))  # Reshape et ajout à la liste
+            start = end
+
+        for param, new_param in zip(self.parameters(), new_parameters_list):
+            param.data.copy_(new_param)
+
+    def get_grad(self):
+        grad_param = []
+        for param in self.parameters():
+           if param.requires_grad:
+               grad_param.append(param.grad.reshape(-1))
+        return torch.cat(grad_param)
+
+
+def neural_exp_para_func(dim_geo,nb_neurones):
+    network = NeuralNetwork(dim_geo,nb_neurones)
+    network.init_id()
+    def exp_para_net(param_metric,t0,p0,v0,w_i,t_i):
+        #print(network.extrait_param())
+        network.import_param(param_metric)
+        para_lin = exp_para_lin(param_metric,t0,p0,v0,w_i,t_i)
+        para_net = network.forward(para_lin)
+        return para_net
+    return exp_para_net,network
+    
